4964d0358d127b97c56badf7d00b6b95b297790f
dovgaloleksii <dovgaloleksii@gmail.com>
2019-06-23 13:24:52 +0300

    Add mock for get_external_service function

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 import datetime
 import unittest
+from unittest.mock import patch
 
 import server
+
+
+def get_external_service(date, external_service_url):
+    if external_service_url == server.temperature_service_url:
+        return {
+            "temp": 5.879879879879889,
+            "date": date
+        }
+
+    elif external_service_url == server.windspeed_service_url:
+        return {
+            "north": -17.989980201472466,
+            "west": 16.300917971882726,
+            "date": date
+        }
 
 
 class ServerTestCase(unittest.TestCase):
@@ -10,7 +26,8 @@ class ServerTestCase(unittest.TestCase):
         server.app.config['TESTING'] = True
         self.app = server.app.test_client()
 
-    def test_speeds(self):
+    @patch('server.get_external_service', side_effect=get_external_service)
+    def test_speeds(self, *args):
         data = self.app.get('/speeds', follow_redirects=True)
         self.assertEqual(data.status_code, 400, "If get speeds without query params server should return "
                                                 "400 status code")
@@ -18,7 +35,8 @@ class ServerTestCase(unittest.TestCase):
         data = self.app.get('/speeds?start=2018-08-01T00:00:00Z&end=2018-08-07T00:00:00Z', follow_redirects=True)
         self.assertEqual(data.status_code, 200)
 
-    def test_date_range(self):
+    @patch('server.get_external_service', side_effect=get_external_service)
+    def test_date_range(self, *args):
         data = self.app.get('/speeds?start=2018-08-01T00:00:00Z&end=2018-08-07T00:00:00Z', follow_redirects=True)
         speeds_list = data.json
         date_range = [speed_info['date'] for speed_info in speeds_list]
@@ -28,7 +46,8 @@ class ServerTestCase(unittest.TestCase):
         self.assertNotIn('2018-08-08T00:00:00Z', date_range, "Next day should not be in date range list")
         self.assertEqual(len(date_range), 7, "Number of date in list should be equals 7")
 
-    def test_error_cases_in_query_param(self):
+    @patch('server.get_external_service', side_effect=get_external_service)
+    def test_error_cases_in_query_param(self, *args):
         data = self.app.get('/speeds?start=2018-08-01T00:00:00Z', follow_redirects=True)
         self.assertEqual(data.json['message'], 'Not valid datetime string for `end` query param')
         self.assertEqual(data.status_code, 400, "If get speeds without end query param server should return "
@@ -45,12 +64,13 @@ class ServerTestCase(unittest.TestCase):
                                                 " get 400 status code")
 
     def test_unavailable_day(self):
-        future_day = (datetime.datetime.now() + datetime.timedelta(days=10)).strftime(format='%Y-%m-%dT%H:%M:%SZ')
+        future_day = (datetime.datetime.now() + datetime.timedelta(days=10)).strftime('%Y-%m-%dT%H:%M:%SZ')
         data = self.app.get(f'/speeds?start={future_day}&end={future_day}', follow_redirects=True)
         self.assertEqual(data.json['message'], f'no sample found for date {future_day}')
         self.assertEqual(data.status_code, 404)
 
-    def test_fields_in_response(self):
+    @patch('server.get_external_service', side_effect=get_external_service)
+    def test_fields_in_response(self, *args):
         data = self.app.get('/speeds?start=2018-08-01T00:00:00Z&end=2018-08-07T00:00:00Z', follow_redirects=True)
         first_element = data.json[0]
         self.assertTrue(first_element.get('north', False), 'Speeds endpoint must return `north` field')
